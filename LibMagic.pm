be313d45f758f3c438e8530b9b7634b951937111
Andreas Fitzner <andreas.fitzner@fv-berlin.de>
2008-08-06 09:06:57 +0000

    Import of FITZNER/File-LibMagic-0.89 from CPAN.
    gitpan-cpan-distribution: File-LibMagic
    gitpan-cpan-version:      0.89
    gitpan-cpan-path:         FITZNER/File-LibMagic-0.89.tgz
    gitpan-cpan-author:       FITZNER
    gitpan-cpan-maturity:     released

--- LibMagic.pm
+++ LibMagic.pm
@@ -29,7 +29,7 @@ our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
 
 our @EXPORT = qw( );
 
-our $VERSION = '0.88';
+our $VERSION = '0.89';
 
 sub AUTOLOAD {
     # This AUTOLOAD is used to 'autoload' constants from the constant()
